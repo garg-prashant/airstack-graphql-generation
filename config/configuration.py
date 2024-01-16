a7ca6864ecfb59911e738be0bba3925fada0f672
Prashant Garg <prashant.garg.ind@gmail.com>
2024-01-16 18:09:13 +0530

    configuration parameter default values updated

--- config/configuration.py
+++ config/configuration.py
@@ -77,27 +77,27 @@ OUTPUT_LENGTH = {
 TEMPERATURE = {
     "min_temperature": 0.0,
     "max_temperature": 1.0,
-    "default_temperature": 0.01,
+    "default_temperature": 0.10,
     "step": 0.01
 }
 
 TOP_P = {
     "min_val": 0.0,
     "max_val": 1.0,
-    "default_val": 0.01,
+    "default_val": 0.20,
     "step": 0.05
 }
 
 TOP_K = {
     "min_val": 0,
     "max_val": 100,
-    "default_val": 20,
+    "default_val": 4,
 }
 
 REPETITION_PENALTY = {
     "min_val": 1.0,
     "max_val": 2.0,
-    "default_val": 1.3,
+    "default_val": 1.1,
     "step": 0.1
 }
 
